2f57700430ac85d7bcd3fd0965852b09e6b89622
Erick Chacon <cherickdev@gmail.com>
2022-09-29 17:04:44 -0400

    Initial Commit

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Erick Chacon on 9/29/22.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
