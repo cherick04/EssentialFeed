ac136f811ab8ecaf8976179cfcf169c20868c352
Erick Chacon <cherickdev@gmail.com>
2022-10-28 18:34:17 -0400

    Add empty project for prototype

---
+++ Prototype/Prototype/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  Prototype
+//
+//  Created by Erick Chacon on 10/28/22.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
---
+++ Prototype/Prototype/SceneDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  SceneDelegate.swift
+//  Prototype
+//
+//  Created by Erick Chacon on 10/28/22.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+}
+
