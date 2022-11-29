6d7cb8c7e87f18abf2c336a81c0b6fc3e2c05022
Erick Chacon <cherickdev@gmail.com>
2022-11-29 11:58:58 -0500

    Organize EssentialFeediOSTests folder

---
+++ EssentialFeed/EssentialFeediOSTests/Shared UI/ListSnapshotTests.swift
@@ -0,0 +1,13 @@
+//
+//  ListSnapshotTests.swift
+//  EssentialFeediOSTests
+//
+//  Created by Erick Chacon on 11/29/22.
+//
+
+import XCTest
+
+final class ListSnapshotTests: XCTestCase {
+
+    
+}
